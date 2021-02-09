214ec043110dd6dad7130d8a16770e4b66e36bd8
Bianca-Cristiana Caluian <bianca.caluian12@gmail.com>
2021-02-09 21:08:06 +0200

    updated security

--- src/main/java/com/echipa3/backend/controllers/AnnouncementController.java
+++ src/main/java/com/echipa3/backend/controllers/AnnouncementController.java
@@ -1,13 +1,11 @@
 package com.echipa3.backend.controllers;
 
-import com.echipa3.backend.dtos.AnnouncementDTO;
 import com.echipa3.backend.entities.Announcement;
 import com.echipa3.backend.services.IAnnouncementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -19,17 +17,7 @@ public class AnnouncementController {
     IAnnouncementService service;
 
     @PostMapping()
-    public Announcement save(@RequestBody AnnouncementDTO announcementDto){
-        Announcement announcement = new Announcement();
-        announcement.setTitle(announcementDto.getTitle());
-        announcement.setShortDescription(announcementDto.getShortDescription());
-        announcement.setCompanyId(1L);
-        announcement.setDescriptionId(1L);
-        announcement.setPublishedDate(new Date());
-        announcement.setImportance(announcementDto.getImportance());
-        announcement.setApprovedForPublishing(announcementDto.getApprovedForPublishing());
-        announcement.setImageId(1L);
-        //announcement.setApprovedForPublishing(announcementDto.getApprovedForPublishing());
+    public Announcement save(@RequestBody Announcement announcement){
         service.saveOrUpdate(announcement);
         return announcement;
     }
