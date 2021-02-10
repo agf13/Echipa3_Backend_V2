a265e01a9f52ab29548e467d0a625ebdc17f0480
Bianca-Cristiana Caluian <bianca.caluian12@gmail.com>
2021-02-10 14:46:21 +0200

    added type to announcement dto

---
+++ src/main/java/com/echipa3/backend/dtos/AnnouncementDto.java
@@ -0,0 +1,128 @@
+package com.echipa3.backend.dtos;
+
+import com.echipa3.backend.entities.Description;
+import com.echipa3.backend.entities.Image;
+import com.echipa3.backend.entities.Tag;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+public class AnnouncementDto implements Serializable {
+    private String title;
+    private Image image;
+    private Description description;
+    private String shortDescription;
+    private Date publishedDate;
+    private boolean isPinned;
+    private boolean approvedForPublishing;
+    private String link;
+    private Set<Tag> tags;
+    private Long companyId;
+    private String type;
+
+    public AnnouncementDto(String title, Image image, Description description, String shortDescription, Date publishedDate, boolean isPinned, boolean approvedForPublishing, String link, Set<Tag> tags, Long companyId, String type) {
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.shortDescription = shortDescription;
+        this.publishedDate = publishedDate;
+        this.isPinned = isPinned;
+        this.approvedForPublishing = approvedForPublishing;
+        this.link = link;
+        this.tags = tags;
+        this.companyId = companyId;
+        this.type = type;
+    }
+
+    public AnnouncementDto() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public void setDescription(Description description) {
+        this.description = description;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public boolean isApprovedForPublishing() {
+        return approvedForPublishing;
+    }
+
+    public void setApprovedForPublishing(boolean approvedForPublishing) {
+        this.approvedForPublishing = approvedForPublishing;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- src/main/java/com/echipa3/backend/services/AnnouncementServiceImpl.java
+++ src/main/java/com/echipa3/backend/services/AnnouncementServiceImpl.java
@@ -1,7 +1,7 @@
 package com.echipa3.backend.services;
 
 import com.echipa3.backend.entities.Announcement;
-import com.echipa3.backend.repositories.IRepoAnnouncement;
+import com.echipa3.backend.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +14,18 @@ public class AnnouncementServiceImpl implements IAnnouncementService{
 
     @Autowired
     IRepoAnnouncement repository;
+    @Autowired
+    IRepoContest repoContest;
+    @Autowired
+    IRepoCourse repoCourse;
+    @Autowired
+    IRepoInternship repoInternship;
+    @Autowired
+    IRepojob repojob;
+    @Autowired
+    IRepoOther repoOther;
+    @Autowired
+    IRepoScholarship repoScholarship;
 
     @Override
     public List<Announcement> getAll() {
@@ -33,5 +45,27 @@ public class AnnouncementServiceImpl implements IAnnouncementService{
     @Override
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    @Override
+    public String getType(Long id) {
+        if(repoContest.findById(id).isPresent())
+            return "contest";
+        if(repoCourse.findById(id).isPresent()){
+            return "course";
+        }
+        if(repoInternship.findById(id).isPresent()){
+            return "internship";
+        }
+        if(repojob.findById(id).isPresent()){
+            return "job";
+        }
+        if(repoOther.findById(id).isPresent()){
+            return "other";
+        }
+        if(repoScholarship.findById(id).isPresent()){
+            return "scholarship";
+        }
+        return "not found";
     }
 }
